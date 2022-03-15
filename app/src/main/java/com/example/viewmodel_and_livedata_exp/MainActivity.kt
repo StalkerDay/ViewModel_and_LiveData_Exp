a390bb42015b73fc2831f42514bb6062a7e3fc3c
Вася Пупкин <Stalker_Day@bigmir.net>
2022-03-15 19:08:48 +0200

    Добавлено ViewModelFactory.

--- app/src/main/java/com/example/viewmodel_and_livedata_exp/MainActivity.kt
+++ app/src/main/java/com/example/viewmodel_and_livedata_exp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.viewmodel_and_livedata_exp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.example.viewmodel_and_livedata_exp.Models.MainActivityViewModelFactory
 import com.example.viewmodel_and_livedata_exp.ViewModels.MainVM
 import com.example.viewmodel_and_livedata_exp.databinding.ActivityMainBinding
 
@@ -10,14 +11,17 @@ import com.example.viewmodel_and_livedata_exp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     private lateinit var viewModel: MainVM
+    private lateinit var viewModelFactory: MainActivityViewModelFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel = ViewModelProvider(this)[MainVM::class.java]
+        viewModelFactory = MainActivityViewModelFactory(125)
+        viewModel = ViewModelProvider(this, viewModelFactory)[MainVM::class.java]
 
         binding.countText.text = viewModel.getCurrentCount().toString()
         binding.button.setOnClickListener {
--- app/src/main/java/com/example/viewmodel_and_livedata_exp/ViewModels/MainVM.kt
+++ app/src/main/java/com/example/viewmodel_and_livedata_exp/ViewModels/MainVM.kt
@@ -2,8 +2,8 @@ package com.example.viewmodel_and_livedata_exp.ViewModels
 
 import androidx.lifecycle.ViewModel
 
-class MainVM : ViewModel() {
-    private var count: Int = 0
+class MainVM(startingCount: Int) : ViewModel() {
+    private var count: Int = startingCount
 
     fun getUpdatedCount(): Int {
         return ++count
